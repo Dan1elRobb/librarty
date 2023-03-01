7c780e147cc9da1fad81ab0d6c7cad893c6426eb
dan1elrobb <danrob0910@highgateschool.org.uk>
2023-03-01 19:50:32 +0000

    upodated w corrections - now runs but nothing goes into the tables - im sure its a simple enough fix but i cannot see it

--- models.py
+++ models.py
@@ -4,48 +4,66 @@ from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
-writer = Table("writer",
-               Base.metadata,
-               Column('book_id', ForeignKey('book.book_id')),
-               Column('writerid', ForeignKey("writer.writerid")),
-               UniqueConstraint('book_id', 'writerid')
-               )
+book_writer = Table("book_writer",
+                    Base.metadata,
+                    Column('book_id', ForeignKey('book.book_id')),
+                    Column('writer_id', ForeignKey("writer.writer_id")),
+                    UniqueConstraint('book_id', 'writer_id')
+                    )
 
-loan = Table("loan",
-             Base.metadata,
-             Column('book_id', ForeignKey('book.book_id')),
-             Column('person_id', ForeignKey("person.person_id")),
-             UniqueConstraint('book_id', 'person_id')
-             )
+book_loan = Table("book_loan",
+                  Base.metadata,
+                  Column('book_id', ForeignKey('book.book_id')),
+                  Column('person_id', ForeignKey("person.person_id")),
+                  UniqueConstraint('book_id', 'person_id')
+                  )
 
 
 class Book(Base):
     __tablename__ = "book"
-    book = Column(Integer, primary_key=True, autoincrement=True)
+    book_id = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(String)
-    publisher_id = Column(Integer, ForeignKey("publisher.publisher_id"))
+    isbn = Column(String, unique=True)
+    num_pages = Column(Integer)
     publication_date = Column(Date)
-    isbn = Column(String)
+    publisher_id = Column(Integer, ForeignKey("publisher.publisher_id"))
+
     writers = relationship("Writer",
-                           secondary=writer)
+                           secondary=book_writer,
+                           order_by='(Writer.writer_name)')
 
     borrowers = relationship("Person",
-                             secondary=loan)
+                             secondary=book_loan,
+                             order_by='(Person.name)')
+
+    publisher = relationship("Publisher")
 
 
 class Publisher(Base):
     __tablename__ = "publisher"
-    publisher = Column(Integer, primary_key=True, autoincrement=True)
-    p_name = Column(String)
+    publisher_id = Column(Integer, primary_key=True, autoincrement=True)
+    publisher_name = Column(String)
+    books = relationship("Book")
+
+
 
 
 class Writer(Base):
     __tablename__ = "writer"
-    Writer = Column(Integer, primary_key=True, autoincrement=True)
-    w_name = Column(String)
+    writer_id = Column(Integer, primary_key=True, autoincrement=True)
+    writer_name = Column(String)
+
+    works = relationship("Book",
+                         secondary=book_writer)
+
 
 
 class Person(Base):
     __tablename__ = "person"
-    person = Column(Integer, primary_key=True, autoincrement=True)
+    person_id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String)
+    membership_expiry = Column(Boolean)
+
+    on_loan = relationship("Book",
+                           secondary=book_loan,
+                           order_by='(Book.title)')
--- write.py
+++ write.py
@@ -1,3 +1,4 @@
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from models import Book, Publisher, Writer, Person
@@ -5,16 +6,26 @@ from random import randint
 from datetime import date
 
 publishers = [
-    Publisher(p_name="Max"),
-    Publisher(p_name="Seb"),
-    Publisher(p_name="ChaNab"),
-    Publisher(p_name='Black and White')
+    Publisher(publisher_name="Macmillan"),
+    Publisher(publisher_name="Simon & Schuster"),
+    Publisher(publisher_name="Penguin"),
+    Publisher(publisher_name="Harper Collins")
 ]
 
 books = [
+    Book(title="Pride and Prejudice",
+         isbn="".join([str(randint(0, 9)) for _ in range(13)]),
+         num_pages=279,
+         publication_date=date(1813, 2, 23),
+         ),
+    Book(title="Normal People",
+         isbn="".join([str(randint(0, 9)) for _ in range(13)]),
+         num_pages=266,
+         publication_date=date(2018, 5, 12),
+         ),
     Book(title="How to Train your dragon",
-         isbn=(randint(999999999999,9999999999999)),
-         num_pages=randint(200,1000),
+         isbn=(randint(999999999999, 9999999999999)),
+         num_pages=randint(200, 1000),
          publication_date=date(1813, 2, 23),
          ),
     Book(title="AH flipping guide",
@@ -29,35 +40,41 @@ books = [
          )
 ]
 
-authors = [
+
+writers = [
+    Writer(writer_name="Jane Eyre"),
     Writer(w_name="Superman"),
     Writer(w_name="batman"),
     Writer(w_name="The flash"),
 ]
 
 people = [
-    Person(name="Sebastian Burbidge"),
-    Person(name="Dave James"),
-    Person(name="Stephen Spoerri"),
-    Person(name="Ariella G"),
-    Person(name="Merris Amos"),
-    Person(name="Daniel Robb")
+    Person(name="Sebastian Burbidge", membership_expiry=False),
+    Person(name="Jon Rahm", membership_expiry=False),
+    Person(name="Oli Zeidler", membership_expiry=False),
+    Person(name="Arthur Delport", membership_expiry=False),
+    Person(name="Catherine Johnson", membership_expiry=False),
+    Person(name="Clive Newsend", membership_expiry=False)
 ]
 
-books[0].publisher = publishers[0]
-books[1].publisher = publishers[1]
-books[2].authors.append(authors[2])
-books[2].authors.append(authors[3])
-books[2].publisher = publishers[0]
-books[3].authors.append(authors[3])
-books[3].publisher = publishers[2]
+books[randint(1,4)].writers.append(writers[0])
+books[randint(1,4)].publisher = publishers[0]
+books[randint(1,4)].writers.append(writers[1])
+books[randint(1,4)].publisher = publishers[1]
+books[randint(1,4)].writers.append(writers[2])
+books[randint(1,4)].writers.append(writers[3])
+books[randint(1,4)].publisher = publishers[0]
+books[randint(1,4)].writers.append(writers[3])
+books[randint(1,4)].publisher = publishers[2]
+books[randint(1,4)].writers.append(writers[3])
+books[randint(1,4)].publisher = publishers[3]
 
-
-books[3].borrowers.append(people[1])
-books[3].borrowers.append(people[4])
-books[0].borrowers.append(people[1])
-books[2].borrowers.append(people[2])
-books[2].borrowers.append(people[0])
+books[randint(1,4)].borrowers.append(people[5])
+books[randint(1,4)].borrowers.append(people[1])
+books[randint(1,4)].borrowers.append(people[4])
+books[randint(1,4)].borrowers.append(people[1])
+books[randint(1,4)].borrowers.append(people[2])
+books[randint(1,4)].borrowers.append(people[0])
 
 engine = create_engine('sqlite:///library.sqlite', echo=True)
 
